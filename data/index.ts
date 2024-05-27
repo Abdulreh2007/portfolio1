1b17145359f4416de0f21cd8f4f26b713ca2b884
AbdulRehman2040 <146017169+AbdulRehman2040@users.noreply.github.com>
2024-05-27 22:17:53 +0500

    c

---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 ",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Eye Clinic Bradford UK ",
+      des: "Eye Clinic Bradford UK is an online platform for booking appointments and accessing eye care services. I create this with next.js, tailwindcss, typescript, threeJS",
+      img: "/my-1.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", ],
+      link: "https://eyeclinicbradforduk.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Abdul's blog with sanity  ",
+      des: "A simple blog website which I created with sanity and an Admin page ",
+      img: "/my-4.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg",  '/sanity.png'],
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Cafe Hawaii",
+      des: "A Cafe based website where you can book table or order food or see the cafe speacilites",
+      img: "/my-5.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg",  "/c.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "An absolute pleasure to work with! Delivered top-notch solutions with incredible efficiency and creativity",
+      name: "Alex Martinez",
+      title: "Creative Director",
+    
+    },
+    {
+      quote:
+        "A true professional who goes above and beyond to ensure the success of every project. Highly recommended!",
+      name: "Jessica Lee",
+      title: "Product Manager",
+    },
+    {
+      quote:
+        "Exceptional skills and dedication. Transformed our vision into a reality with impressive technical expertise.",
+      name: "Mark Thompson",
+      title: "CEO",
+    },
+    {
+      quote:
+        "Brilliant problem-solver with a keen eye for detail. Consistently exceeded our expectations on every project.",
+      name: "Jhony sins",
+      title: "Sowftwere Enginer",
+    },
+   
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Developer",
+      desc: "Assisted in the development of a web-based platform using Next.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance  Dev Project",
+      desc: "Led the dev for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 2,
+      title: "Fast API",
+      desc: "Also working in fast API with help of python and uses kafka",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
