a3c88dc31f775d19ad510f2ab1dba8b8a7171814
AbdulRehman2040 <fahadkhan1tea@gmail.com>
2024-07-08 11:49:51 +0500

    n

--- data/index.ts
+++ data/index.ts
@@ -96,11 +96,11 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "",
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "/ui.aiimg.com",
     },
   ];
   
